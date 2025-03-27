2dc1ef23ee09c9bbbf7d1fc8717606ba71753d78
Andi Rueckel <andirueckel@users.noreply.github.com>
2025-03-27 14:44:17 +0100

    fix: unique plugin names for `2025`/`2026` blueprint

--- site/plugins/2025-blueprint/index.php
+++ site/plugins/2025-blueprint/index.php
@@ -2,7 +2,7 @@
 
 use Kirby\Cms\App as Kirby;
 
-Kirby::plugin('medienhaus/default', [
+Kirby::plugin('medienhaus/2025-blueprint', [
     'blueprints' => [
         'pages/2025' => function ($kirby) {
             return include __DIR__ . '/blueprints/pages/2025.php';
--- site/plugins/2026-blueprint/index.php
+++ site/plugins/2026-blueprint/index.php
@@ -2,7 +2,7 @@
 
 use Kirby\Cms\App as Kirby;
 
-Kirby::plugin('medienhaus/default', [
+Kirby::plugin('medienhaus/2026-blueprint', [
     'blueprints' => [
         'pages/2026' => function ($kirby) {
             return include __DIR__ . '/blueprints/pages/2026.php';

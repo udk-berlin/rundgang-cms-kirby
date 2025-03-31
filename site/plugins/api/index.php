f0275ee709a69c6a3943bffef5e580e529d5b1e3
Andi Rueckel <andirueckel@users.noreply.github.com>
2025-03-31 17:16:53 +0200

    draft: change path and file names of json files

--- site/plugins/api/index.php
+++ site/plugins/api/index.php
@@ -6,19 +6,19 @@ Kirby::plugin('medienhaus/api', [
             [
                 'pattern' => '2025/contexts',
                 'action' => function () {
-                    return asset('assets/udk_contexts_2025.json')->read();
+                    return asset('assets/2025/contexts.json')->read();
                 },
             ],
             [
                 'pattern' => '2025/formats',
                 'action' => function () {
-                    return asset('assets/udk_formats_2025.json')->read();
+                    return asset('assets/2025/formats.json')->read();
                 },
             ],
             [
                 'pattern' => '2025/locations',
                 'action' => function () {
-                    return asset('assets/udk_locations_2025.json')->read();
+                    return asset('assets/2025/locations.json')->read();
                 },
             ],
         ],

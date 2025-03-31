ab4b7ee775f4d22505c023a59844edc8fbebe2ea
Andi Rueckel <andirueckel@users.noreply.github.com>
2025-03-31 17:17:14 +0200

    draft: add provisoric boilerplate for 2026

--- site/plugins/api/index.php
+++ site/plugins/api/index.php
@@ -21,6 +21,26 @@ Kirby::plugin('medienhaus/api', [
                     return asset('assets/2025/locations.json')->read();
                 },
             ],
+            /*
+            [
+                'pattern' => '2026/contexts',
+                'action' => function () {
+                    return asset('assets/2026/contexts.json')->read();
+                },
+            ],
+            [
+                'pattern' => '2026/formats',
+                'action' => function () {
+                    return asset('assets/2026/formats.json')->read();
+                },
+            ],
+            [
+                'pattern' => '2026/locations',
+                'action' => function () {
+                    return asset('assets/2026/locations.json')->read();
+                },
+            ],
+             */
         ],
     ],
 ]);

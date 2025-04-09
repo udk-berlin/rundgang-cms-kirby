c330b43b6e84edf22435162a0fd93e2deecf7b94
Marcel <79279756+aofn@users.noreply.github.com>
2025-04-09 13:58:59 +0200

    feat: - add API endpoint to expose kirbys filterBy function for pages - add request queries for filter type and value

--- site/plugins/api/index.php
+++ site/plugins/api/index.php
@@ -2,6 +2,14 @@
 
 Kirby::plugin('medienhaus/api', [
     'api' => [
+        'data' => [
+            'filterPagesBy' => function ($filter, $value) {
+                return kirby()->site()->pages()->filterBy(
+                    $filter,
+                    $value,
+                );
+            },
+        ],
         'routes' => [
             [
                 'pattern' => '2025/contexts',
@@ -21,6 +29,21 @@ Kirby::plugin('medienhaus/api', [
                     return asset('assets/2025/locations.json')->read();
                 },
             ],
+            [
+                // APi endpoint to filter pages by a specific field
+                // e.g. /api/2025/filterBy?filter=format&value=project_presentation
+                // more information on filterBy: https://getkirby.com/docs/reference/objects/cms/pages/filter-by
+                'pattern' => '2025/filterPagesBy',
+                'action' => function () {
+                    $api = kirby()->api();
+                    $filter = $api->requestQuery('filter', '');
+                    $value = $api->requestQuery('value', '');
+                    return [
+                        $this->filterPagesBy($filter, $value),
+                    ];
+                },
+            ],
+
             /*
             [
                 'pattern' => '2026/contexts',

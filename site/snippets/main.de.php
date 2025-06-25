f335e4190b92b767bfb17763804e223bd615128a
Andi Rueckel <andirueckel@users.noreply.github.com>
2025-06-25 10:51:01 +0200

    chore: update texts as proposed

--- site/snippets/main.de.php
+++ site/snippets/main.de.php
@@ -1,11 +1,29 @@
     <main>
       <p>
-        Das Eintragen, Editieren und Verwalten von Inhalten ist bis zum Beginn des Rundgangs am <em>Freitag, den 18. Juli 2025, um 16:00 Uhr</em> möglich. Vielen Dank für euer Mitwirken und viel Vergnügen am Rundgang 2025.
+        Ihr wollt eure Projekte auf der Rundgang-Plattform hochladen?
       </p>
       <p>
-        Für den <a href="/panel" rel="nofollow noreferrer" target="_blank">Login</a> zum Rundgang CMS benutzt eure <code>@udk-berlin.de</code> oder <code>@intra.udk-berlin.de</code> Mail-Adresse und das zugehörige <a href="https://www.oase.udk-berlin.de/udk-oase-nutzeraccount/passwort-vergessen/" rel="external nofollow noreferrer" target="_blank">Passwort</a>.
+        Hier könnt ihr alle analogen und digitalen Veranstaltungen und Projekte für den Rundgang 2025 eintragen, bearbeiten und verwalten!
       </p>
       <p>
-        Die Inhalte werden <em>dann</em> auf der <a href="https://rundgang.udk-berlin.de/" rel="external nofollow noreferrer" target="_blank">Rundgang 2025 Website</a> gezeigt.</p>
+        Ab dem <em>2. Juli 2025</em> könnt ihr eure Inhalte einpflegen. Zur gleichen Zeit geht die <a href="https://rundgang.udk-berlin.de/" rel="external nofollow noreferrer" target="_blank">Rundgang-Website 2025</a> online und ihr könnt die Inhalte live prüfen.
+      </p>
+      <p>
+        Die Eintragungen können aber auch danach noch bis zum Start des Rundgangs am Freitag, den <em>18. Juli 2025</em>, um 16 Uhr eingetragen und ergänzt werden. Ab Beginn des Rundgangs sind keine Eintragungen und Änderungen mehr möglich.
+      </p>
+      <hr />
+      <p>
+        Für den <a href="/panel" rel="nofollow noreferrer" target="_blank">Login</a> nutzt bitte euren <code>@udk-berlin.de</code> oder <code>@intra.udk-berlin.de</code> Account und das zugehörige <a href="https://www.oase.udk-berlin.de/udk-oase-nutzeraccount/passwort-vergessen/" rel="external nofollow noreferrer" target="_blank">Passwort</a>.
+      </p>
+      <p>
+        Die eingetragenen und von euch veröffentlichten Inhalte werden anschließend auf der <a href="https://rundgang.udk-berlin.de/" rel="external nofollow noreferrer" target="_blank">Rundgang-Website 2025</a> gezeigt.</p>
+      </p>
+      <hr />
+      <p>
+        Hier findet ihr weitere <a href="https://www.udk-berlin.de/en/rundgang/" rel="external nofollow noreferrer" target="_blank">Informationen</a> zum Rundgang 2025 sowie die <a href="https://www.udk-berlin.de/rundgang/2025/orte-und-oeffnungszeiten/" rel="external nofollow noreferrer" target="_blank">Öffnungszeiten</a> der teilnehmenden Standorte.</p>
+      </p>
+      <hr />
+      <p>
+        Vielen Dank für eure Beiträge – wir freuen uns auf eure Projekte und wünschen viel Freude beim Mitgestalten des Rundgangs 2025!
       </p>
     </main>
--- site/snippets/main.en.php
+++ site/snippets/main.en.php
@@ -1,11 +1,29 @@
     <main>
       <p>
-        You can create, edit, and manage your content until the start of the Rundgang on <em>Friday, July 18, 2025, at 16:00 p.m.</em> — thank you so much for your participation, and enjoy your time at the Rundgang 2025.
+        Do you want to upload your projects to the Rundgang-Platform?
       </p>
       <p>
-        For the <a href="/panel" rel="nofollow noreferrer" target="_blank">login</a> to the Rundgang CMS, you need your <code>@udk-berlin.de</code> or <code>@intra.udk-berlin.de</code> mail address and the associated <a href="https://www.oase.udk-berlin.de/udk-oase-nutzeraccount/passwort-vergessen/" rel="external nofollow noreferrer" target="_blank">password</a>.
+        Here you can enter, edit and manage all analog and digital events and projects for the Rundgang 2025!
       </p>
       <p>
-        The content will <em>then</em> be presented via the <a href="https://rundgang.udk-berlin.de/" rel="external nofollow noreferrer" target="_blank">Rundgang 2025 Website</a>.</p>
+        Starting on <em>July 2, 2025</em>, you can submit your content. At the same time, the <a href="https://rundgang.udk-berlin.de/" rel="external nofollow noreferrer" target="_blank">Rundgang website 2025</a> will be put online and you can preview your content.
+      </p>
+      <p>
+        However, entries can still be submitted and edited until the Rundgang starts on Friday, <em>July 18, 2025</em> at 4 pm. After this time, it will no longer be possible to submit any more entries or edit already submitted ones.
+      </p>
+      <hr />
+      <p>
+        To <a href="/panel" rel="nofollow noreferrer" target="_blank">log in</a>, please use your <code>@udk-berlin.de</code> or </code>@intra.udk-berlin.de</code> account and the corresponding <a href="https://www.oase.udk-berlin.de/udk-oase-nutzeraccount/passwort-vergessen/" rel="external nofollow noreferrer" target="_blank">password</a>.
+      </p>
+      <p>
+        Any content you enter and mark for publication will be shown on the <a href="https://rundgang.udk-berlin.de/" rel="external nofollow noreferrer" target="_blank">Rundgang website 2025</a>.
+      </p>
+      <hr />
+      <p>
+        Here you can find more <a href="https://www.udk-berlin.de/en/rundgang/" rel="external nofollow noreferrer" target="_blank">informationen</a> about the Rundgang 2025 as well as the <a href="https://www.udk-berlin.de/rundgang/2025/orte-und-oeffnungszeiten/" rel="external nofollow noreferrer" target="_blank">opening hours</a> for the participating locations.
+      </p>
+      <hr />
+      <p>
+        Thank you very much for your entries — we are looking forward to your projects that will shape the Rundgang 2025!
       </p>
     </main>

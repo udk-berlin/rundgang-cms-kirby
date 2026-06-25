b698a856dcfa1d62283abc8193ca04b66948f679
Florian Weber <florian@weber.world>
2026-06-25 18:54:27 +0200

    Add a twist of FSR Medienhaus

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -9,4 +9,12 @@
   <body>
     <header>
       <h1>UdK Berlin — Rundgang Content Management System (CMS)</h1>
+      <div class="frame" id="frame" style="width: 115.2333px; height: 52.6668px; left: 62ch; top: 55px; transform: rotate(0.142638rad);background: #ff0;position: absolute;border: 3.5px solid #00f;transform-origin: center center;user-select: none;display: inline-flex;justify-content: center;align-items: center;padding: 0;margin: 0;/*! transform: rotate(-5deg); */position: fixed;bottom: 40vh;right: 65px;padding: 10px 10px;" data-angle="-0.19263780301733147">
+        <h1 id="text" style="font-size: 21.0212px;">reloaded</h1>
+        <div class="handle" data-pos="tl"></div>
+        <div class="handle" data-pos="tr"></div>
+        <div class="handle" data-pos="bl"></div>
+        <div class="handle" data-pos="br"></div>
+        <div class="handle rotate"></div>
+      </div>
     </header>

79f63124d85124dd59b9f392379d9b12b1274e44
Florian Weber <florian@weber.world>
2026-06-25 18:53:12 +0200

    Copy update

--- site/snippets/main.de.php
+++ site/snippets/main.de.php
@@ -5,25 +5,12 @@
       <p>
         Hier könnt ihr alle analogen und digitalen Veranstaltungen und Projekte für den Rundgang 2026 eintragen, bearbeiten und verwalten!
       </p>
-      <p>
-        Ab dem <em>2. Juli 2026</em> könnt ihr eure Inhalte einpflegen. Zur gleichen Zeit geht die <a href="https://rundgang.udk-berlin.de/" rel="external nofollow noreferrer" target="_blank">Rundgang-Website 2026</a> online und ihr könnt die Inhalte live prüfen.
-      </p>
-      <p>
-        Die Eintragungen können aber auch danach noch bis zum Start des Rundgangs am Freitag, den <em>17. Juli 2026</em>, um 16 Uhr eingetragen und ergänzt werden. Ab Beginn des Rundgangs sind keine Eintragungen und Änderungen mehr möglich.
-      </p>
       <hr />
       <p>
         Für den <a href="/panel" rel="nofollow noreferrer" target="_blank">Login</a> nutzt bitte euren <code>@udk-berlin.de</code> oder <code>@intra.udk-berlin.de</code> Account und das zugehörige <a href="https://www.oase.udk-berlin.de/udk-oase-nutzeraccount/passwort-vergessen/" rel="external nofollow noreferrer" target="_blank">Passwort</a>.
       </p>
-      <p>
-        Die eingetragenen und von euch veröffentlichten Inhalte werden anschließend auf der <a href="https://rundgang.udk-berlin.de/" rel="external nofollow noreferrer" target="_blank">Rundgang-Website 2026</a> gezeigt.</p>
-      </p>
       <hr />
       <p>
         Hier findet ihr weitere <a href="https://www.udk-berlin.de/en/rundgang/" rel="external nofollow noreferrer" target="_blank">Informationen</a> zum Rundgang 2026 sowie die <a href="https://www.udk-berlin.de/rundgang/2025/orte-und-oeffnungszeiten-2/" rel="external nofollow noreferrer" target="_blank">Öffnungszeiten</a> der teilnehmenden Standorte.</p>
-      </p>
-      <hr />
-      <p>
-        Vielen Dank für eure Beiträge – wir freuen uns auf eure Projekte und wünschen viel Freude beim Mitgestalten des Rundgangs 2026!
       </p>
     </main>

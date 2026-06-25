cc3b6ff321a42167a19da1216be0b95ea4750f89
Florian Weber <florian@weber.world>
2026-06-25 18:37:51 +0200

    Update site title to remove hyphen in "UdK-Berlin"

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <link rel="stylesheet" type="text/css" href="/assets/css/index.css" />
-    <title>UdK-Berlin — Rundgang CMS</title>
+    <title>UdK Berlin — Rundgang CMS</title>
   </head>
   <body>
     <header>

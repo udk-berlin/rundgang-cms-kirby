7806d630bd1aed797a17f0c71b54bbb90aeebbf8
Florian Weber <florian@weber.world>
2026-07-17 11:12:46 +0200

    Fix typos in login instructions and Rundgang 2026 information

--- site/snippets/main.en.php
+++ site/snippets/main.en.php
@@ -7,10 +7,10 @@
       </p>
       <hr />
       <p>
-        To <a href="/panel" rel="nofollow noreferrer" target="_blank">log in</a>, please use your <code>@udk-berlin.de</code> or </code>@intra.udk-berlin.de</code> account and the corresponding <a href="https://www.oase.udk-berlin.de/udk-oase-nutzeraccount/passwort-vergessen/" rel="external nofollow noreferrer" target="_blank">password</a>.
+        To <a href="/panel" rel="nofollow noreferrer" target="_blank">log in</a>, please use your <code>@udk-berlin.de</code> or <code>@intra.udk-berlin.de</code> account and the corresponding <a href="https://www.oase.udk-berlin.de/udk-oase-nutzeraccount/passwort-vergessen/" rel="external nofollow noreferrer" target="_blank">password</a>.
       </p>
       <hr />
       <p>
-        Here you can find more <a href="https://www.udk-berlin.de/en/rundgang/" rel="external nofollow noreferrer" target="_blank">informationen</a> about the Rundgang 2026 as well as the <a href="https://www.udk-berlin.de/rundgang/2025/orte-und-oeffnungszeiten-2/" rel="external nofollow noreferrer" target="_blank">opening hours</a> for the participating locations.
+        Here you can find more <a href="https://www.udk-berlin.de/en/rundgang/" rel="external nofollow noreferrer" target="_blank">information</a> about the Rundgang 2026 as well as the <a href="https://www.udk-berlin.de/rundgang/2025/orte-und-oeffnungszeiten-2/" rel="external nofollow noreferrer" target="_blank">opening hours</a> for the participating locations.
       </p>
     </main>
